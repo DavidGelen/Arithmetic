df8061277fd57ad53383800c96640f8948f33db8
David <18588744489@163.com>
2019-03-23 20:03:58 +0800

    Update

--- app/src/main/java/com/wwe/insert_sort/InsertSort.java
+++ app/src/main/java/com/wwe/insert_sort/InsertSort.java
@@ -10,8 +10,12 @@ package com.wwe.insert_sort;
 public class InsertSort {
     public static void main(String[] args) {
         int[] arr = {3,44,38,5,47,15,36,26,27,2,46,4,19,50,48};
+        sort(arr);
+    }
+
+    private static void insertSort(int[] arr) {
         int i,j;
-        for(i = 1; i< arr.length; i++){
+        for(i = 1; i < arr.length; i++){
             int temp = arr[i];
             for(j = i - 1; j >= 0 && arr[j]> temp; j--){
                 arr[j+1] = arr[j];
@@ -19,6 +23,19 @@ public class InsertSort {
             arr[j+1] = temp;
         }
         traverse(arr);
+    }
+
+    public static void sort(int[] numbers){
+        for (int i = 1; i < numbers.length; i++) {
+            int currentNumber = numbers[i];
+            int j = i - 1;
+            while (j >= 0 && numbers[j] > currentNumber) {
+                numbers[j + 1] = numbers[j];
+                j--;
+            }
+            numbers[j+1] = currentNumber;
+        }
+        traverse(numbers);
     }
 
     private static void traverse(int[] arr) {
