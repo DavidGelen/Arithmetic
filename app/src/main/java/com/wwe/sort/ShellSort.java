ffbea5d545c1768ced1a4e75332d049c29463dfa
David <zhangbiao001@chinasofti.com>
2019-04-07 16:47:45 +0800

    Update

--- app/src/main/java/com/wwe/sort/ShellSort.java
+++ app/src/main/java/com/wwe/sort/ShellSort.java
@@ -15,7 +15,7 @@ public class ShellSort {
 
         //5,10，1，第一次gap=4，取出间隔数
         //其实如果数组很长，用4作间隔也不合适，移动次数会很多
-        //效率最高的间隔序列算法是怎么样的呢？
+        //效率最高的间隔序列算法是怎么样的呢？h = 3*h + 1 这个是knuth公式
         shellSort(arr);
 
         //写完之后用datacheck对一下,验证算法的正确性
@@ -23,6 +23,24 @@ public class ShellSort {
     }
 
     public static void shellSort(int[] arr) {
+        int h = 1;
+        while(h <= arr.length/3) {
+            h = 3*h + 1;
+        }
+        for(int gap = h; gap > 0; gap = (gap - 1)/3) {
+            for(int i = gap; i < arr.length; i++) {
+                for(int j = i; j > gap - 1; j -= gap) {
+                    if(arr[j] < arr[j - gap]) {
+                        swap(arr, j,j-gap);
+                    }
+                }
+            }
+        }
+    }
+
+    public static void shellSortHalf(int[] arr) {
+
+        //arr.length >> 1 等价于 arr.length / 2
         for(int gap = arr.length >> 1; gap > 0; gap /= 2) {
             for(int i = gap; i < arr.length; i++) {
                 for(int j = i; j > gap - 1; j -= gap) {
