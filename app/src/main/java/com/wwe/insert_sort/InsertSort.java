fc3f7ff97ac683b94cbe167186f122876590b7ff
David <zhangbiao001@chinasofti.com>
2019-04-07 15:19:52 +0800

    Update

--- app/src/main/java/com/wwe/insert_sort/InsertSort.java
+++ app/src/main/java/com/wwe/insert_sort/InsertSort.java
@@ -11,33 +11,28 @@ public class InsertSort {
     public static void main(String[] args) {
         int[] arr = {3,44,38,5,47,15,36,26,27,2,46,4,19,50,48};
         sort(arr);
-    }
-
-    private static void insertSort(int[] arr) {
-        int i,j;
-        for(i = 1; i < arr.length; i++){
-            int temp = arr[i];
-            for(j = i - 1; j >= 0 && arr[j]> temp; j--){
-                arr[j+1] = arr[j];
-            }
-            arr[j+1] = temp;
-        }
         traverse(arr);
     }
 
-    public static void sort(int[] numbers){
-        for (int i = 1; i < numbers.length; i++) {
-            int currentNumber = numbers[i];
-            int j = i - 1;
-            while (j >= 0 && numbers[j] > currentNumber) {
-                numbers[j + 1] = numbers[j];
-                j--;
+    private static void sort(int[] arr) {
+
+        //为什么i不是0，因为0前面没数啦
+        for(int i = 1; i < arr.length; i++) {
+            //j是从右往左排，所以是一个做减法的操作
+            for(int j = i; j > 0; j--) {
+                if(arr[j] < arr[j - 1]) {
+                    swap(arr, j);
+                }
             }
-            numbers[j+1] = currentNumber;
         }
-        traverse(numbers);
     }
 
+    private static void swap(int[] arr, int j) {
+        int temp = arr[j - 1];
+        arr[j - 1] = arr[j];
+        arr[j] = temp;
+    }
+    
     private static void traverse(int[] arr) {
         for(int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
